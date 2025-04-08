9edc8a667b3b6b2b7f7536d05852be023eb67874
Iris Goldstein <iris.goldstein0@gmail.com>
2025-04-08 18:21:27 -0400

    Renamed the two caesar cypher files

--- Cryptology/CaesarCypher.java
+++ Cryptology/CaesarCypher.java
@@ -1,49 +1,206 @@
-// a basic caesar cypher, that offsets letters by a certain circular offset
-public class CaesarCypher extends Cypher
+import java.security.SecureRandom;
+
+public class CaesarCypher
 {
-	// var dec
-	private final int offset;
+	/// INSTANCE VARIABLES
+	
+	private final String original;
+	private String encrypted;
+	// visible: original message and rotation can be accessed
+	// secret: if true, message cannot be checked
+	private final boolean visible, secret; 
+	private final int rotation;
 
-	// constructor
-	public CaesarCypher (String originalMessage, boolean messageViewable, int offset)
+	/// CONSTRUCTORS
+	
+	/**
+	 * @param originalMessage The message to be encrypted
+	 * @param rotation The secret key; how many positions in the alphabet to rotate.
+	 * 					For instance, a value of 2 would make a -> c, etc.
+	 * @param visible Whether or not the original message and key can be freely viewed, useful for decryption practice.
+	 * @param secret Whether or not a message can be checked to see if it is the original method. <br>
+	 * 					Makes brute force attacks involving generating sequential possiblities and testing them directly infeasible. <br>
+	 * 					Turn off to allow for verification.
+	 */
+	public CaesarCypher (String originalMessage, int rotation, boolean visible, boolean secret)
 	{
-		super(originalMessage, messageViewable, new CaesarParameters(offset));
-		this.offset = offset;
+		original = originalMessage;
+		this.rotation = rotation;
+		if (secret)
+			visible = false;
+		this.visible = visible;
+		this.secret = secret;
+		applyRotation();
 	}
 
-	// apply the encoding by rotating each character by the specified offset
-	// keeps capitalization, rotates digits between themselves, and ignores special characters
-	public String applyEncoding (String message, Parameters encodingParameters)
+	public CaesarCypher (String originalMessage, boolean visible, boolean secret)
 	{
-		String encodedMessage = "";
-
-		for (char chr : message.toCharArray())
-		{
-			if (Character.isUpperCase(chr))
-			{
-				//////////////////////////////////// continue here, implement wraparound
-			}
-			else if (Character.isLowerCase(chr))
-			{
-
-			}
-			else if (Character.isDigit(chr))
-			{
-				
-			}
-		}
+		original = originalMessage;
+		SecureRandom secRand = new SecureRandom();
+		this.rotation = secRand.nextInt(1, 25);
+		if (secret)
+			visible = false;
+		this.visible = visible;
+		this.secret = secret;
+		applyRotation();
 	}
 
-	// gets the offset if the original message is viewable
-	public int getOffset() throws IllegalAccessException
+	public CaesarCypher (String originalmessage, int rotation, boolean visible)
 	{
-		if (messageViewable())
+		this(originalmessage, rotation, visible, false);
+	}
+
+	public CaesarCypher (String originalMessage, int rotation)
+	{
+		this(originalMessage, rotation, false, false);
+	}
+
+	public CaesarCypher (String originalmessage, boolean visible)
+	{
+		this(originalmessage, visible, false);
+	}
+
+	public CaesarCypher (String originalMessage)
+	{
+		this(originalMessage, false, false);
+	}
+
+	/// PUBLIC INTERFACE
+	
+	/**
+	 * @return The message after encryption
+	 */
+	public String getEncryptedMessage()
+	{
+		return encrypted;
+	}
+
+	/**
+	 * @return The encrypted message
+	 */
+	@Override
+	public String toString()
+	{
+		return encrypted;
+	}
+
+	/**
+	 * Checks whether the provided String is the same as the original message. <br>
+	 * NOTE: Will only work if the cypher was not declared secret upon creation
+	 * 
+	 * @throws SecurityException Throws an exception if the cypher is secret, and no additional information may be revealed.
+	 * @param guess The string to check against the original string
+	 * @return A boolean indicating whether the guess matches the original string
+	 */
+	public boolean checkValue (String guess) throws SecurityException
+	{
+		if (secret)
 		{
-			return offset;
+			throw new SecurityException("This cypher is secret, and none of the contents may be accessed");
 		}
 		else
 		{
-			throw new IllegalAccessException("Offset cannot be viewed, it's a secret :3");
+			return original.equals(guess);
 		}
+	}
+
+	/**
+	 * @return Whether the contents of the cypher are freely visible.
+	 */
+	public boolean isVisible () {return visible;}
+
+	/**
+	 * @return Whether any aspects of the cypher other than the encrypted message are accessible.
+	 */
+	public boolean isSecret () {return secret;}
+
+	/**
+	 * If the cypher is visible, returns the original message, otherwise throws an exception. <br>
+	 * NOTE: will only work if the cypher is declared visible upon creation
+	 * 
+	 * @return The original message, if visible
+	 * @throws SecurityException Throws an exception is the cypher is not set to visible
+	 */
+	public String getOriginalMessage () throws SecurityException
+	{
+		if (visible)
+		{
+			return original;
+		}
+		else
+		{
+			throw new SecurityException("This cypher is not visible, so the orinal message cannot be accessed");
+		}
+	}
+
+	/**
+	 * If the cypher is visible, returns the rotation, otherwise throws an exception. <br>
+	 * NOTE: will only work if the cypher is declared visible upon creation
+	 * 
+	 * @return The rotation factor, if visible
+	 * @throws SecurityException Throws an exception is the cypher is not set to visible
+	 */
+	public int getRotation () throws SecurityException
+	{
+		if (visible)
+		{
+			return rotation;
+		}
+		else
+		{
+			throw new SecurityException("This cypher is not visible, so the rotation cannot be accessed");
+		}
+	}
+
+	/// BACK END METHODS
+
+	/**
+	 * Set up the encrypted message. <br>
+	 * Rotates letters by the set rotation, preserving case, and leaves other characters alone
+	 */
+	private void applyRotation()
+	{
+		encrypted = "";
+
+		for (char ch : original.toCharArray())
+		{
+			if (Character.isLowerCase(ch))
+			{
+				encrypted += (char) boundedRotation((int) ch, rotation, (int) 'a', (int) 'z' + 1);
+			}
+			else if (Character.isUpperCase(ch))
+			{
+				encrypted += (char) boundedRotation((int) ch, rotation, (int) 'A', (int) 'Z' + 1);
+			}
+			else 
+			{
+				encrypted += ch;
+			}
+		}
+	}
+
+	/**
+	 * Rotate a value by a specified number of positions, constrained within a range. <br>
+	 * If the value exceeds the given range, it will wrap around.
+	 * 
+	 * @param val The value that is being rotated. Usually the index or ASCII value of a character
+	 * @param rot The number of places to rotate by
+	 * @param min The minimum bound of the interval (inclusive). If the value, after rotation, is lower than this value, it will be wrapped around
+	 * @param max The maximum bound of the interval (exclusive). If the value, after rotation, is greater than or equal to, it will be wrapped around
+	 */
+	private int boundedRotation(int val, int rot, int min, int max)
+	{
+		int range = max - min;
+
+		// rot %= range;
+
+		val += rot;
+
+		if (val < min)
+			val += range;
+		if (val >= max)
+			val -= range;
+
+		return val;
 	}
 }
