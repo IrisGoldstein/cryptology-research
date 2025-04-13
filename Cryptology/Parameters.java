592d00a773ae766340d29ce3e1c8d02afa10900a
Iris Goldstein <iris.goldstein0@gmail.com>
2025-04-13 16:12:48 -0400

    Expanded Paramter abstract class

--- Cryptology/Parameters.java
+++ Cryptology/Parameters.java
@@ -1,3 +1,21 @@
 // class to encapsulate a set of parameters to instantiate a cypher
 // should be implemented as a separate subclass for any given cypher type
-public abstract class Parameters {}
+public abstract class Parameters 
+{
+	// List of types of cyphers that can be created
+	public static enum CipherType 
+	{
+		CAESAR,
+		MODIFIED_CAESAR,
+		SUBSTITUTION,
+		MODIFIED_SUBSTITUTION,
+		RSA
+	};
+
+	private final CipherType type;
+
+	public Parameters (CipherType type)
+	{
+		this.type = type;
+	}
+}
